8e330815ef2a3093198b8895968d6665e30f6ddd
DUA_Designs <dua99mech.engr@gmail.com>
2024-02-19 15:00:26 +0530

    third

--- src/imageComponents.js
+++ src/imageComponents.js
@@ -9,5 +9,9 @@ import Pic4 from './images/weather Pics/pic4.png';
 import Pic5 from './images/weather Pics/pic5.png';
 import Pic6 from './images/weather Pics/pic6.png';
 import infotrixs from './images/Infotrixs.png';
+import Roxiler from './images/roxiler.png';
+import Roxiler1 from './images/roxiler1.png';
+import Roxiler2 from './images/roxiler2.png';
+import Roxiler3 from './images/roxiler3.png';
 
-export {dicegame,weatherAppDemo,quizAppDemo,DD,Pic1,Pic2,Pic3,Pic4,Pic5,Pic6,infotrixs};
+export {dicegame,weatherAppDemo,quizAppDemo,DD,Pic1,Pic2,Pic3,Pic4,Pic5,Pic6,infotrixs,Roxiler,Roxiler1,Roxiler2,Roxiler3};
