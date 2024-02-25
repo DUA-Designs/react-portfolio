daab9f2c7e02f9616f66cc3467a98a962c11286f
DUA_Designs <dua99mech.engr@gmail.com>
2024-02-25 16:23:45 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Main } from './portfolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+ <React.StrictMode>
   <Main/>
-  
+  </React.StrictMode>
 );
 
  
--- src/portfolio.js
+++ src/portfolio.js
@@ -4,11 +4,12 @@ import emailjs from '@emailjs/browser';
 import  {dicegame,weatherAppDemo,quizAppDemo,DD,Pic1,Pic2,Pic3,Pic4,Pic5,Pic6,infotrixs, Roxiler, Roxiler1, Roxiler2, Roxiler3,shorts} from './media';
 
  
+
  const projects=[
-  {icon:<i class="fa-solid fa-play"></i>,git:"https://github.com/DUA-Designs/HelloAR",demo:"https://dua-designs.github.io/HelloAR/",completion:"February 2024",name:"Shorts App",desc:"Shorts App allows you to add videos that are oriented vertically and also scroll through the available ones. The working is similar to youtube shorts. This project is made with React App and it is a coding challenge given to me by HelloAR.",video:shorts},
-  {icon:<i class="fa-solid fa-table-columns"></i>,git:"https://github.com/DUA-Designs/roxilerBackend",demo:"https://roxilerfront-end.onrender.com/",completion:"February 2024",name:"Transaction Dashboard",desc:"Functional Sales Dashboard featuring table for showing the items, sections for Statistics and barChart for price Ranges. This project is based on MERN Stack and it is a coding challenge given to me by  GEEKS-FOR-GEEKS for ROXILER company recruitment.",carousel:[Roxiler,Roxiler1,Roxiler2,Roxiler3]},
-  {icon:<i class="fi fi-ss-cloud-code"></i>,git:"https://github.com/DUA-Designs/WeatherDashboard",demo:"https://dua-designs.github.io/WeatherDashboard/",completion:"January 2024",name:"Weather Dashboard",desc:"Fully functional Weather Dashboard offering wide range of data. Provide input details to get weather data.",carousel:[Pic1,Pic2,Pic3,Pic4,Pic5,Pic6]},
- {icon:<i class="fa-solid fa-cloud"></i>,git:"https://github.com/DUA-Designs/weatherAPI",demo:"https://dua-designs.github.io/weatherAPI/",completion:"January 2024",name:"Current Weather",desc:"The Weather App uses a weather API to get the information using javascrit fetch. The data is then displayed as innerHTML of the  html elements. You will have to type the city name for which you want to get current weather details.",video:weatherAppDemo,linkedIn:"https://www.linkedin.com/posts/uday-aravind-dasari-385636225_infotrixs-infotrixsteam-infotrixsinternship-activity-7149757564606427136-yUzN?utm_source=share&utm_medium=member_desktop"},
+  {icon:<i className="fa-solid fa-play"></i>,git:"https://github.com/DUA-Designs/HelloAR",demo:"https://dua-designs.github.io/HelloAR/",completion:"February 2024",name:"Shorts App",desc:"Shorts App allows you to add videos that are oriented vertically and also scroll through the available ones. The working is similar to youtube shorts. This project is made with React App and it is a coding challenge given to me by HelloAR.",video:shorts},
+  {icon:<i className="fa-solid fa-table-columns"></i>,git:"https://github.com/DUA-Designs/roxilerBackend",demo:"https://roxilerfront-end.onrender.com/",completion:"February 2024",name:"Transaction Dashboard",desc:"Functional Sales Dashboard featuring table for showing the items, sections for Statistics and barChart for price Ranges. This project is based on MERN Stack and it is a coding challenge given to me by  GEEKS-FOR-GEEKS for ROXILER company recruitment.",carousel:[Roxiler,Roxiler1,Roxiler2,Roxiler3]},
+  {icon:<i className="fi fi-ss-cloud-code"></i>,git:"https://github.com/DUA-Designs/WeatherDashboard",demo:"https://dua-designs.github.io/WeatherDashboard/",completion:"January 2024",name:"Weather Dashboard",desc:"Fully functional Weather Dashboard offering wide range of data. Provide input details to get weather data.",carousel:[Pic1,Pic2,Pic3,Pic4,Pic5,Pic6]},
+ {icon:<i className="fa-solid fa-cloud"></i>,git:"https://github.com/DUA-Designs/weatherAPI",demo:"https://dua-designs.github.io/weatherAPI/",completion:"January 2024",name:"Current Weather",desc:"The Weather App uses a weather API to get the information using javascrit fetch. The data is then displayed as innerHTML of the  html elements. You will have to type the city name for which you want to get current weather details.",video:weatherAppDemo,linkedIn:"https://www.linkedin.com/posts/uday-aravind-dasari-385636225_infotrixs-infotrixsteam-infotrixsinternship-activity-7149757564606427136-yUzN?utm_source=share&utm_medium=member_desktop"},
  {icon:"",git:"https://github.com/DUA-Designs/quizApp",demo:"https://dua-designs.github.io/quizApp/",completion:"December 2023",name:"Quiz App",desc:"This is a template of a quiz App that I have designed. This quiz contains multiple choice questions and algorithms with code executable interface. To execute the code I have implemented a Java compiler API and UI has been designed with react,html,css. This personal project was a real tough one to create and I guess every challenge accomplished is a unique piece of development in me.",video:quizAppDemo},
  {link:"https://dua-designs.github.io/diceGame",name:"Dice Game",img:dicegame,tech:"Built with HTML, CSS, Bootstrap, JavaScript, React"},{link:"https://dua-designs.github.io/toDoApp/",img:"https://dua-designs.github.io/myportfolio/images/toDoApp.png",name:"ToDo List App",tech:"Built with HTML, CSS, Bootstrap, JavaScript, React"},
 {link:"https://dua-designs.github.io/bootstrapTemplate/",name:"Bootstrap Template",img:"	https://dua-designs.github.io/portfolio/images/bootInspan.png",tech:"Built with HTML, CSS, Bootstrap"},
@@ -28,7 +29,7 @@ import  {dicegame,weatherAppDemo,quizAppDemo,DD,Pic1,Pic2,Pic3,Pic4,Pic5,Pic6,in
  
  const category=["Programming Languages","Web Development Technologies","Front-end Technologies"];
  const subCat={"Programming Languages":["JavaScript","Java"],"Web Development Technologies":["React"],"Front-end Technologies":["HTML","CSS","Bootstrap","jQuery"]};
- const individuals={"JavaScript":{icon:<i class="fa-brands fa-js"></i>,lvl:80},"Java":{icon:<i class="fa-brands fa-java"></i>,lvl:60},"HTML":{icon:<i class="fa-brands fa-html5"></i>,lvl:90},"CSS":{icon:<i class="fa-brands fa-css3-alt"></i>,lvl:90},"React":{icon:<i class="fa-brands fa-react"></i>,lvl:70},"Bootstrap":{icon:<i class="fa-brands fa-bootstrap"></i>,lvl:70},"jQuery":""};
+ const individuals={"JavaScript":{icon:<i className="fa-brands fa-js"></i>,lvl:80},"Java":{icon:<i className="fa-brands fa-java"></i>,lvl:60},"HTML":{icon:<i className="fa-brands fa-html5"></i>,lvl:90},"CSS":{icon:<i className="fa-brands fa-css3-alt"></i>,lvl:90},"React":{icon:<i className="fa-brands fa-react"></i>,lvl:70},"Bootstrap":{icon:<i className="fa-brands fa-bootstrap"></i>,lvl:70},"jQuery":""};
  
  
  
@@ -240,7 +241,7 @@ useEffect(()=>{
                    document.getElementById(id).innerHTML=`${i} %`;
                    document.getElementById(`line${sub}`).style.width=`${i}%`;
                    let comparator=window.screen.width<=768?document.getElementById(`lan${sub}`).offsetWidth+20 :document.getElementById(`lan${sub}`).offsetWidth+30;
-                   console.log(comparator);
+                   
                    if(document.getElementById(`line${sub }`).offsetWidth>comparator){
                     document.getElementById(id).classList.add("showMe");
                     document.getElementById(id).style.width=`${i}%`;
@@ -254,7 +255,7 @@ useEffect(()=>{
     <nav className="navbar navbar-expand-lg position-fixed    z-3 "id="mrNav">
         <div className="container-fluid d-flex  position-relative  " id="navContainer">
           <a className="navbar-brand col-1" href="#dua" ><img src={DD} className="col-lg-5 col-md-5 col-sm-8 " id="logo" alt="D_D"></img>  D.U.A <span className="sepe">|</span><span className="desi"> DESIGNS</span></a>
-          <button className=" btn " id="theme" onClick={handleBrightness} title={bright?"Switch to Lightmode":"Switch To Darkmode"}>{bright?<i className="fi fi-bs-brightness"></i>:<i class="fa-solid fa-moon"></i>}</button>
+          <button className=" btn " id="theme" onClick={handleBrightness} title={bright?"Switch to Lightmode":"Switch To Darkmode"}>{bright?<i className="fi fi-bs-brightness"></i>:<i className="fa-solid fa-moon"></i>}</button>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon  " id="navToggler"></span>
           </button>
@@ -291,7 +292,7 @@ useEffect(()=>{
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 my-2 py-3  rounded hoverStyle" id="aboutMe"> 
                <h1><span className="fs-6">Hi, I am </span>Uday Aravind.</h1>
                 <h3><em>Aspiring Full Stack Developer.</em></h3>
-                <h6>{"I create <"} <span id="textChange"></span> {"/>"} </h6>
+                <h6>{"I create <"} <span id="textChange"></span><span id="blinkingCursor">&nbsp;</span> {"/>"} </h6>
             </div>
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 my-2 p-1  rounded hoverStyle"><p className="fs-6">Welcome to my Portfolio. Over here I showcase my
                 extensive expertise to the world, including a diverse
@@ -304,14 +305,14 @@ useEffect(()=>{
                   <h3 className="">Quote of the day</h3>
 
                   {quoteCheck===1?<div className="col-12"><i className="fa-solid fa-quote-left"></i> <blockquote id="quote" className="d-inline p-3" >{quote[0]}</blockquote> <i className="fa-solid fa-quote-right"></i> 
-                  <p className="text-end" id="author" >{quote[1]}</p></div>:<div class="lds-ripple pb-5 "><div></div><div></div></div>} 
+                  <p className="text-end" id="author" >{quote[1]}</p></div>:<div className="lds-ripple pb-5 "><div></div><div></div></div>} 
                 </div>
         </div>
 
       </section>
       <section id="skills" className="py-5">
         <div className="container-fluid">
-          <h1 className="p-4 shadow  w-75  border rounded position-relative mx-auto  mb-5 " id="mySkills"><span className="  position-absolute translate-middle top-0 start-50  shadow  bg-body-tertiary border rounded-circle text-danger" id="Icon"><i class="fa-solid fa-brain"></i></span>Tech Stack</h1>
+          <h1 className="p-4 shadow  w-75  border rounded position-relative mx-auto  mb-5 " id="mySkills"><span className="  position-absolute translate-middle top-0 start-50  shadow  bg-body-tertiary border rounded-circle text-danger" id="Icon"><i className="fa-solid fa-brain"></i></span>Tech Stack</h1>
           <div className="row py-4  ">
            {/*skills.map((item,index)=> <div className="col-lg-8 col-md-10 col-sm-12 col-xs-12 mx-auto p-2 d-flex justify-content-around my-1 skills-container" key={index} ><div className="col-2 skills-icons"  >{item.icons}</div><div className="col-4 skill-type"  ><h4>{item.Language}</h4></div><div  className="col-4 bar"><div className="track" style={{width:`${item.level}%`}}></div></div></div>)*/}
              <div className=" col-11 mx-auto  text-start   bg-light rounded" id="categories">
@@ -344,8 +345,8 @@ useEffect(()=>{
         
       
         <div className="row p-2 "  >
-        {projects.map((item)=> item.desc? 
-                                  <div className="col-12 border rounded   p-lg-4 p-md-2 p-sm-1 p-xs-0 shadow bg-body-tertiary  d-flex flex-wrap  my-2 p-1">
+        {projects.map((item,index)=> item.desc? 
+                                  <div className="col-12 border rounded   p-lg-4 p-md-2 p-sm-1 p-xs-0 shadow bg-body-tertiary  d-flex flex-wrap  my-2 p-1" key={index}>
                                     <div className="col-lg-7 col-md-8 col-sm-12 col-xs-12 p-2  ">
                                       {item.video?<video autoPlay    muted loop  className="col-12 border rounded "  ><source src={item.video}></source></video>:
                                              <div id="carouselExampleSlidesOnly" className="carousel slide  " data-bs-ride="carousel">
@@ -355,8 +356,8 @@ useEffect(()=>{
                                                </div>
                                                {item.carousel.map((carouselItem,caroIndex)=>{
                                                 if(caroIndex>0){
-                                                  return (<div class="carousel-item"  >
-                                                 <img src={carouselItem} class="d-block w-100 border rounded" alt="..."/>
+                                                  return (<div className="carousel-item"  key={caroIndex}>
+                                                 <img src={carouselItem} className="d-block w-100 border rounded" alt="..."/>
                                                </div>);
                                                }
                                                else{
@@ -371,12 +372,12 @@ useEffect(()=>{
                                     </div>
                                   <div className="col-lg-5 col-md-4 col-sm-12 col-xs-12 p-lg-4 p-md-2 p-sm-1 p-xs-0 my-2" id="projectDescription">
                                     <h4 className=" ">{item.name}({item.completion}) {item.icon}</h4><p className="col-12 p-lg-4 p-md-2 p-sm-1 p-xs-0 ">{item.desc}</p>
-                                    {item.linkedIn?<div className="p-1 m-1"><a className="btn btn-dark" href={item.linkedIn}>Working Detail <i class="fa-solid fa-circle-info"></i></a></div>:""}
-                                    <div className="d-flex justify-content-around col-12 "><a href={item.git} className="btn btn-dark col-5">Code <i class="fa-brands fa-github"></i></a><a href={item.demo} className="btn btn-dark col-5">Live Demo <i class="fa-solid fa-arrow-up-right-from-square"></i></a></div></div>
+                                    {item.linkedIn?<div className="p-1 m-1"><a className="btn btn-dark" href={item.linkedIn}>Working Detail <i className="fa-solid fa-circle-info"></i></a></div>:""}
+                                    <div className="d-flex justify-content-around col-12 "><a href={item.git} className="btn btn-dark col-5">Code <i className="fa-brands fa-github"></i></a><a href={item.demo} className="btn btn-dark col-5">Live Demo <i className="fa-solid fa-arrow-up-right-from-square"></i></a></div></div>
                                   </div>:
 
 
-                                  <div className="col-lg-4 col-md-6 col-sm-6 col-xs-10 my-2 position-relative " id="box">
+                                  <div className="col-lg-4 col-md-6 col-sm-6 col-xs-10 my-2 position-relative " id="box" key={index}>
                                     <div className="shadow border border rounded p-2 bg-white"> 
                                    <div id="tech"  className="position-absolute"><a   href={item.link}>{item.tech}</a></div> 
                                     <a href={item.link}> <img  src={item.img}  alt="project_Image"  className="img-fluid"/>
